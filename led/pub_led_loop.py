647fb581a2e6ecfc137d11a2c63a6369ed3bfa3a
gea <gangely7@gmail.org>
2018-04-24 16:28:30 +0200

    renamed BROKER ..

--- led/pub_led_loop.py
+++ led/pub_led_loop.py
@@ -1,5 +1,25 @@
+### pub_led_loop.py ###
+## espp/led /led-dht /led-dht-ds
+## gea20180424
+
+## history
+# 20180424 parameters BROKER ..
+
+
+## user and local parameters ##
+BROKER = "hc1"
+PORT = 1883
+TOPIC = "esp32/led"
+QOS = 1
+RETAIN = True
+## not used
+#user = "yourUser"
+#password = "yourPassword"
+
+
 import paho.mqtt.client as mqttClient
 import time
+
  
 def on_connect(client, userdata, flags, rc):
  
@@ -15,16 +35,11 @@ def on_connect(client, userdata, flags, rc):
         print("Connection failed")
  
 Connected = False   #global variable for the state of the connection
- 
-broker_address= "hc1"
-port = 1883
-#user = "yourUser"
-#password = "yourPassword"
- 
-client = mqttClient.Client("Python")               #create new instance
+  
+client = mqttClient.Client("Python")                #create new instance
 #client.username_pw_set(user, password=password)    #set username and password
-client.on_connect= on_connect                      #attach function to callback
-client.connect(broker_address, port=port)          #connect to broker
+client.on_connect= on_connect                       #attach function to callback
+client.connect(BROKER, port=PORT)                   #connect to broker
  
 client.loop_start()        #start the loop
  
@@ -35,7 +50,7 @@ try:
     while True:
  
         value = input('Enter the message: ')
-        client.publish("esp32/led",value,qos=1,retain=True)
+        client.publish(TOPIC,value,qos=QOS,retain=RETAIN)
  
 except KeyboardInterrupt:
  
